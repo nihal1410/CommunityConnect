9ea8112c1a8117c059049cbfbf5c613fb75ea624
nihal1410 <mahansarianihal@gmail.com>
2024-03-18 03:01:33 +0530

    Some Progress

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '/Users/nihalmahansaria/Desktop/CommunityConnect/my-app/src/components/icons/SearchIcon.png'; // Example path to icon
+import BrightnessIcon from '/Users/nihalmahansaria/Desktop/CommunityConnect/my-app/src/components/icons/BrightnessIcon.png'; // Example path to icon
+import UserIcon from '/Users/nihalmahansaria/Desktop/CommunityConnect/my-app/src/components/icons/UserIcon.png'; // Example path to icon
 
 function Header() {
   return (
     <header className="header">
-      <input type="search" placeholder="Search" className="search-bar"/>
-      { /* Insert your search and brightness icons here */ }
-      <div className="nav-links">
+      <div className="icons">
+        <img src={SearchIcon} alt="Search" />
+        <img src={BrightnessIcon} alt="Brightness" />
+      </div>
+      <div className="navigation">
         <a href="/">Home</a>
         <a href="/store">Store</a>
         <a href="/blog">Blog</a>
-        <button className="login-btn">Login/Register</button>
+      </div>
+      <div className="login-register">
+        <img src={UserIcon} alt="User" />
+        <div>
+          <a href="/login">Login</a>
+          <span>/</span>
+          <a href="/register">Register</a>
+        </div>
       </div>
     </header>
   );
